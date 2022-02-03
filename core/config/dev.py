dc55806dfa3a50468e6b1e7cccd862e6f3563b63
Md Nazmul Hasan <farhapartex@gmail.com>
2022-02-03 19:37:54 +0600

    Project basic setup is done

---
+++ core/config/dev.py
@@ -0,0 +1,20 @@
+from core.config.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
+}
