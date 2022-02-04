48e1bb26b705c7f763dba95db104b99a054063db
Md Nazmul Hasan <farhapartex@gmail.com>
2022-02-04 09:18:07 +0600

    added models in restaurant app

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from restaurant.models import *
 # Register your models here.
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "name")
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("id", "restaurant", "name", "price")
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from core.base_model import BaseAbstractModel
+from user.models import User
 # Create your models here.
+
+
+class Restaurant(BaseAbstractModel):
+    owner = models.ForeignKey(User, related_name="restaurants", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Menu(BaseAbstractModel):
+    restaurant = models.ForeignKey(Restaurant, related_name="menus", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.id)
