b1521f23880c7023c6b5724937d2420390c7c49c
Md Nazmul Hasan <farhapartex@gmail.com>
2022-02-07 23:47:18 +0600

    docker file added

--- core/config/local.py
+++ core/config/local.py
@@ -1,7 +1,7 @@
 from core.config.base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -10,11 +10,11 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "menuvotedb",
+        'USER': "devadmin",
+        'PASSWORD': "Ninja6708",
+        'HOST': "db",
+        'PORT': "5432",
     }
 }
