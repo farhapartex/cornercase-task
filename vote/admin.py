9d72afcd907bf65f84587fb4d9f66cfe8a6eb4ad
Md Nazmul Hasan <farhapartex@gmail.com>
2022-02-07 09:32:50 +0600

    Vote model created

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from vote.models import Vote
 # Register your models here.
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "restaurant", "employee")
--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from core.base_model import BaseAbstractModel
+from restaurant.models import Restaurant
+from user.models import User
 # Create your models here.
+
+
+class Vote(BaseAbstractModel):
+    restaurant = models.ForeignKey(Restaurant, related_name="votes", on_delete=models.DO_NOTHING)
+    employee = models.ForeignKey(User, related_name="employee_votes", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(self.id)
+
+
